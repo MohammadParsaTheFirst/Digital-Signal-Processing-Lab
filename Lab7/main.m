3bb497183430b904d5e66ba0a6eeb92725a43d18
MohammadParsa Dini <mohammadparsadinithefirst@gmail.com>
2025-05-07 21:06:37 +0330

    Update main.m

--- Lab7/main.m
+++ Lab7/main.m
@@ -1,10 +1,11 @@
 %% step 1 -- making the complex signal --
 path = "C:\\Users\\USER\\Desktop\\sdrsharp\\IQ\\2025_05_03\\audio4.Wav";
-[ raw_signal , fs ] = audioread ( path ) ;
-I = raw_signal (: ,1) ;
-Q = raw_signal (: ,2) ;
-complexSignal = I + 1i * Q ;
-% step 2 -- filtering the freqs > 100kHz --
+[ raw_signal , fs ] = audioread ( path ) ;             % the raw signal
+I = raw_signal (: ,1) ;                                % in-phase 
+Q = raw_signal (: ,2) ;                                % quadrature
+complexSignal = I + 1i * Q ;                           % the envelope signal
+
+% -- filtering the freqs > 100kHz --
 lp_cutoff = 1e5;
 lpFilt = designfilt ( 'lowpassfir' , ...
 'PassbandFrequency' , lp_cutoff , ...
@@ -12,12 +13,13 @@ lpFilt = designfilt ( 'lowpassfir' , ...
 'SampleRate' , fs , ...
 'DesignMethod' , 'equiripple') ;
 filteredSignal = filter ( lpFilt , complexSignal ) ;
-% step 3 -- getting the phase, diffferentiating it & removing its DC --
+
+% -- getting the phase, diffferentiating it & removing its DC --
 instPhase = unwrap ( angle ( filteredSignal ) ) ;
 demodulated = diff ( instPhase ) ;
 demodulated = demodulated - mean ( demodulated ) ;
 
-%% step 4 -- getting the (sterio) L+R signal --
+%% step 2 -- getting the (sterio) L+R signal --
 lp_cutoff2 = 1.5e4;
 lpFilt2 = designfilt ( 'lowpassfir' , ...
 'PassbandFrequency' , lp_cutoff2 , ...
@@ -27,19 +29,19 @@ lpFilt2 = designfilt ( 'lowpassfir' , ...
 
 filteredSignal2 = filter ( lpFilt2 , demodulated ) ;
 
-%% step 5 -- decimation of L+R signal --
+%% step 3 -- decimation of L+R signal --
 audio_fs = 44100;
 decimation_factor = 40;%floor ( fs / aud% io_fs ) ;
 audio = decimate(filteredSignal2 , decimation_factor ) ;
 audio = audio / max ( abs ( audio ) ) ;
 
-% step 6 -- playing the L+R signal
+% -- playing the L+R signal
 sound ( audio , fs/decimation_factor ) ; % Play the audio at the target sampling rate
 
-% step 7 --
+% -- showing the frequency contents of teh L+R signal
 pwelch(audio,[] , [], [], fs/decimation_factor)
 
-%% step 8 -- getting & decimating the sterio DSB signal --
+%% step 4 -- getting & decimating the sterio DSB signal --
 
 bp_cutoff1 = 2.3e4; % Lower passband frequency in Hz
 bp_cutoff2 = 5.3e4; % Upper passband frequency in Hz
@@ -57,9 +59,9 @@ filteredSignal3 = filter(bpFilt, demodulated);
 audio_fs = 44100;
 decimation_factor = 40;
 audio2 = decimate(filteredSignal3 , decimation_factor ) ;
-audio2 = audio / max ( abs ( audio2 ) ) ;
+audio2 = audio / max ( abs ( audio2 ) ) ; 
 
-%% step 9 -- extracting the delta and its phase --
+%% step 5 -- extracting the delta and its phase --
 
 pilot_bpFilt = designfilt('bandpassfir', ...
     'PassbandFrequency1', 1.85e4, ...
@@ -71,11 +73,11 @@ pilot_bpFilt = designfilt('bandpassfir', ...
 
 pilotTone = filter(pilot_bpFilt, demodulated);
 
-% 9.2 Get analytic signal & phase of 19kHz
+% --- getting analytic signal & phase of 19kHz
 analyticPilot = hilbert(pilotTone);
 carrier38kHz = cos(2 * angle(analyticPilot));    % generate a 38kHz signal
 
-%% Demodulating the signal
+%% step 6 -- Demodulating the signal
 
 % demodulation (DSB-SC demodulation)
 modulated_filteredSignal3 = filteredSignal3 .* carrier38kHz;  %% the L-R signal
@@ -87,19 +89,50 @@ filteredSignal4 = filter(lpFilt2, modulated_filteredSignal3);
 decimated_filteredSignal4 = decimate(filteredSignal4, decimation_factor);
 decimated_filteredSignal4 = decimated_filteredSignal4 / max(abs(decimated_filteredSignal4));
 
-%% Left & Right signals
+% Left & Right signals
 Left = (decimated_filteredSignal4 + audio)/2;
 Right = (- decimated_filteredSignal4 + audio)/2;
 
 
-%% playing the left song 
-sound(Left,  fs/decimation_factor)
+%% step 7 -- playing the left song --
+sound(Left,  fs/decimation_factor);
+pwelch(Left,[] , [], [], fs/decimation_factor);
 
-%% playning the right song
-sound(Right, fs/decimation_factor)
+%% step 8 -- playing the right song --
+sound(Right, fs/decimation_factor);
+pwelch(Right,[] , [], [], fs/decimation_factor);
 
-%% playing in sterio
+%%  step 9 -- playing in sterio --
 stereoAudio = [Left, Right];
 sound(stereoAudio, fs / decimation_factor);
+pwelch(stereoAudio,[] , [], [], fs/decimation_factor)
 
+%% step 10 -- getting the RDS signal --
+
+bp_cutoff1 = 5.6e4; % Lower passband frequency in Hz
+bp_cutoff2 = 5.8e4; % Upper passband frequency in Hz
+
+bpFilt2 = designfilt('bandpassfir', ...
+    'PassbandFrequency1', bp_cutoff1, ...
+    'StopbandFrequency1', (bp_cutoff1- 1e3), ...
+    'PassbandFrequency2', bp_cutoff2, ...
+    'StopbandFrequency2', (bp_cutoff2 + 1e3), ...
+    'SampleRate', fs, ...
+    'DesignMethod', 'equiripple');
+
+%% step 11 -- getting the RDS signal & decoding it
+filteredSignal5 = filter(bpFilt2, demodulated);
+phase = angle(filteredSignal5);                       
+dphase = diff(unwrap(phase));           
+dpsk_bits = dphase > 0;  
+manchester = 2*dpsk_bits - 1;
+% Group into pairs
+manchester_pairs = reshape(manchester, 2, []);
+% Check transitions: (assuming IEEE 802.3: 1=LOW→HIGH, 0=HIGH→LOW)
+decoded_bits = manchester_pairs(2,:) > manchester_pairs(1,:);
+
+
+%%
+t = 1:length(filteredSignal5):1;
+plot(filteredSignal5, t);
 
